e13e7b785f9bf30061ce15d97ec0e6ff42f482e9
christine parsons <clong9712@gmail.com>
2024-12-06 10:54:41 -0500

    update business information

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,21 +1,21 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
-    "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
-        "country": "US",
-        "mapLink": "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6"
-    },
-    "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
-    },
-    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+  name: "Dr. Matthew Cohen",
+  email: "drmatthewcohen@yahoo.com",
+  phoneForTel: "248-542-3653",
+  phoneFormatted: "(248) 542-3653",
+  address: {
+    lineOne: "Madison Heights Chiropractic Center",
+    lineTwo: "28107 John R",
+    city: "Madison Heights",
+    state: "MI",
+    zip: "48071",
+    country: "US",
+    mapLink: "https://maps.app.goo.gl/xjnbjASCwLA93SZn6",
+  },
+  socials: {
+    facebook: "https://www.facebook.com/",
+    instagram: "https://www.instagram.com/",
+  },
+  //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+  domain: "https://www.example.com",
 };
